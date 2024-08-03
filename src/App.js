7d834feb6d24ba4234f55631f35fa6ed7b46b811
Leo. <erenesaslan@hotmail.com>
2024-08-03 14:04:20 +0300

    update

--- src/App.js
+++ src/App.js
@@ -3,16 +3,19 @@ import Balance from "./components/Balance";
 import IncomeExpenses from "./components/IncomeExpenses";
 import TransactionList from "./components/TransactionList";
 import AddTransaction from "./components/AddTransaction";
+import {GlobalProvider} from "./context/GlobalState";
 
 function App() {
   return (
     <div className="App">
-        <div className='container'>
-            <Balance/>
-            <IncomeExpenses/>
-            <TransactionList/>
-            <AddTransaction/>
-        </div>
+        <GlobalProvider>
+            <div className='container'>
+                <Balance/>
+                <IncomeExpenses/>
+                <TransactionList/>
+                <AddTransaction/>
+            </div>
+        </GlobalProvider>
     </div>
   );
 }
--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -1,11 +1,15 @@
+import Transaction from "./Transaction";
+import { GlobalContext } from "../context/GlobalState";
+import {useContext} from "react";
+
 export default function TransactionList() {
+    const {transactions} = useContext(GlobalContext);
+
     return(
         <div>
             <h3>History</h3>
             <ul className="list">
-                <li className="minus">
-                  Cash <span>-$400</span><button className="delete-btn">x</button>
-                </li>
+                {transactions.map((transaction) => (<Transaction key={transaction.id} transaction={transaction} />))}
             </ul>
         </div>
     )
